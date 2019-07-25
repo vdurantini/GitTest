469ee572b406c6d647d5e1b3df33801c638b6c92
begear2019 <v.durantini@begear.it>
2019-07-25 17:15:59 +0200

    Creato ciclo for per il countdown

--- src/base/TestOne.java
+++ src/base/TestOne.java
@@ -2,9 +2,16 @@ package base;
 
 public class TestOne {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.out.println("This is just a simple test");
+		
+		// Ciclo for per fare il countdown
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+		System.out.println("Go!");
 
 	}
 
